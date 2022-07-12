c0df618ef35a22c08d0f8dfa6d4969814f4a7995
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-07-12 10:43:03 +0400

    Add app-name and template-name flags

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,29 @@
 #! /usr/bin/env node
-const hello = 123
-console.log(hello)
+const fs = require("fs")
+const path = require("path")
+
+const [, , ...flags] = process.argv
+
+const templateNameFlag = flags.find((aFlag) => aFlag.startsWith("template-name="))
+if (templateNameFlag === undefined) {
+  throw new Error(
+    `Template name is not provided. Example: \`npx starter-boilerplate template-name=typescript-react app-name=my-beautiful-app\`.
+Available templates: [typescript-react].`
+  )
+}
+const templateName = templateNameFlag.slice(14)
+
+const appNameFlag = flags.find((aFlag) => aFlag.startsWith("app-name="))
+if (appNameFlag === undefined) {
+  throw new Error(
+    "App name is not provided. Example: `npx starter-boilerplate template-name=typescript-react app-name=my-beautiful-app`."
+  )
+}
+const appName = appNameFlag.slice(9)
+
+const srcDirPath = path.join(__dirname, "..", "templates", templateName)
+const destDirPath = path.join(process.cwd(), appName)
+
+fs.cpSync(srcDirPath, destDirPath, { recursive: true })
+
 console.log("Template created successfully.")
