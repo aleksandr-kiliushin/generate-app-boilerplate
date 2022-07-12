e99eb36e48cf2519cdf6330ced5bda70463c6f95
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-07-12 10:45:05 +0400

    feat: Replace error throw with console error and return

--- bin/index.js
+++ bin/index.js
@@ -2,28 +2,34 @@
 const fs = require("fs")
 const path = require("path")
 
-const [, , ...flags] = process.argv
+const main = () => {
+  const [, , ...flags] = process.argv
 
-const templateNameFlag = flags.find((aFlag) => aFlag.startsWith("template-name="))
-if (templateNameFlag === undefined) {
-  throw new Error(
-    `Template name is not provided. Example: \`npx starter-boilerplate template-name=typescript-react app-name=my-beautiful-app\`.
-Available templates: [typescript-react].`
-  )
+  const templateNameFlag = flags.find((aFlag) => aFlag.startsWith("template-name="))
+  if (templateNameFlag === undefined) {
+    console.error(
+      `Template name is not provided. Example: \`npx starter-boilerplate template-name=typescript-react app-name=my-beautiful-app\`.
+	Available templates: [typescript-react].`
+    )
+    return
+  }
+  const templateName = templateNameFlag.slice(14)
+
+  const appNameFlag = flags.find((aFlag) => aFlag.startsWith("app-name="))
+  if (appNameFlag === undefined) {
+    console.error(
+      "App name is not provided. Example: `npx starter-boilerplate template-name=typescript-react app-name=my-beautiful-app`."
+    )
+    return
+  }
+  const appName = appNameFlag.slice(9)
+
+  const srcDirPath = path.join(__dirname, "..", "templates", templateName)
+  const destDirPath = path.join(process.cwd(), appName)
+
+  fs.cpSync(srcDirPath, destDirPath, { recursive: true })
+
+  console.log("Template created successfully.")
 }
-const templateName = templateNameFlag.slice(14)
 
-const appNameFlag = flags.find((aFlag) => aFlag.startsWith("app-name="))
-if (appNameFlag === undefined) {
-  throw new Error(
-    "App name is not provided. Example: `npx starter-boilerplate template-name=typescript-react app-name=my-beautiful-app`."
-  )
-}
-const appName = appNameFlag.slice(9)
-
-const srcDirPath = path.join(__dirname, "..", "templates", templateName)
-const destDirPath = path.join(process.cwd(), appName)
-
-fs.cpSync(srcDirPath, destDirPath, { recursive: true })
-
-console.log("Template created successfully.")
+main()
