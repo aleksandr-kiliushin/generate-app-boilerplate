9f4f1b3c4de325e103abbb2c3871b298b3136d87
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-07-14 15:39:38 +0400

    feat: Add a story for Button

---
+++ templates/typescript-react/src/components/Button/Button.stories.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+import { ComponentStory, ComponentMeta } from "@storybook/react"
+
+import { Button } from "./index"
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {},
+  component: Button,
+  title: "My components/Button",
+} as ComponentMeta<typeof Button>
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Button> = (args) => {
+  return <Button {...args}>Click me</Button>
+}
+
+export const Default = Template.bind({})
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {}
