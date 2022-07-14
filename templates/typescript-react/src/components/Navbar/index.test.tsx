1dfcf36a364d54c2cafbcf94926c7e8e5ba56930
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-07-14 19:30:03 +0400

    feat: Use HistoryRouter for tests

--- templates/typescript-react/src/components/Navbar/index.test.tsx
+++ templates/typescript-react/src/components/Navbar/index.test.tsx
@@ -4,7 +4,19 @@ import React from "react"
 import { Navbar } from "./index"
 
 describe("<Navbar />", () => {
-  it("it follows to /counter url when Counter link is clicked.", () => {
+  // it("follows to /counter url when Counter link is clicked.", () => {
+  //   render(<Navbar />)
+  //   const linkToCounter = screen.getByRole("link", { name: "Counter" })
+  //   expect(linkToCounter).toBeInTheDocument()
+  // })
+
+  it("renders a link to Home.", () => {
+    render(<Navbar />)
+    const linkToHome = screen.getByRole("link", { name: "Home" })
+    expect(linkToHome).toBeInTheDocument()
+  })
+
+  it("renders a link to Counter.", () => {
     render(<Navbar />)
     const linkToCounter = screen.getByRole("link", { name: "Counter" })
     expect(linkToCounter).toBeInTheDocument()
--- templates/typescript-react/src/utils/testing/render.tsx
+++ templates/typescript-react/src/utils/testing/render.tsx
@@ -2,13 +2,16 @@ import { store } from "#store/index"
 import { RenderOptions, RenderResult, render as rtlRender } from "@testing-library/react"
 import React from "react"
 import { Provider } from "react-redux"
-import { MemoryRouter } from "react-router-dom"
+import { unstable_HistoryRouter as HistoryRouter } from "react-router-dom"
+import { createBrowserHistory } from "history"
 
 const AllTheProviders: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const history = createBrowserHistory()
+
   return (
-    <MemoryRouter>
+    <HistoryRouter history={history}>
       <Provider store={store}>{children}</Provider>
-    </MemoryRouter>
+    </HistoryRouter>
   )
 }
 
