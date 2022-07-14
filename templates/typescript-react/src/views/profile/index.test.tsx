f2785294d0a539ed346bd5bb28441f071f66ecb4
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-07-14 21:08:58 +0400

    refactor: Adds a TODO

--- templates/typescript-react/src/views/profile/index.test.tsx
+++ templates/typescript-react/src/views/profile/index.test.tsx
@@ -7,6 +7,7 @@ describe("<Profile />", () => {
   it("fetches user name and renders it.", async () => {
     render(<Profile />)
     expect(screen.getByText("User name is loading ...")).toBeInTheDocument()
+    // TODO: Mock network request with MSW.
     expect(screen.queryByText("Jackie Chan")).not.toBeInTheDocument()
     expect(await screen.findByText("Jackie Chan")).toBeInTheDocument()
     expect(screen.queryByText("User name is loading ...")).not.toBeInTheDocument()
