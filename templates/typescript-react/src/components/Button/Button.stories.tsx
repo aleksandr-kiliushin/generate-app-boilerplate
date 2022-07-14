4fe6118c152a5913eabf93238f7b5faa00c17562
Aleksandr <aleksandr.kiliushin@gmail.com>
2022-07-14 16:50:03 +0400

    chore: Configure storybook interactions and tests

--- templates/typescript-react/src/components/Button/Button.stories.tsx
+++ templates/typescript-react/src/components/Button/Button.stories.tsx
@@ -1,25 +1,35 @@
+import { expect } from "@storybook/jest"
+import { ComponentMeta, ComponentStory } from "@storybook/react"
+import { userEvent, waitFor, within } from "@storybook/testing-library"
 import React from "react"
-import { ComponentStory, ComponentMeta } from "@storybook/react"
 
 import { Button } from "./index"
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {},
+  argTypes: {
+    onClick: {
+      action: true,
+    },
+  },
   component: Button,
   title: "My components/Button",
 } as ComponentMeta<typeof Button>
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Button> = (args) => {
-  return (
-    <Button {...args} onClick={console.log}>
-      Click me
-    </Button>
-  )
+  return <Button {...args}>Click me</Button>
 }
 
 export const Default = Template.bind({})
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Default.args = {}
+Default.play = async ({ args, canvasElement }) => {
+  const canvas = within(canvasElement)
+  userEvent.click(canvas.getByRole("button"))
+  console.log("args.onClick >>", args.onClick)
+  await waitFor(() => {
+    expect(args.onClick).toHaveBeenCalled()
+  })
+}
